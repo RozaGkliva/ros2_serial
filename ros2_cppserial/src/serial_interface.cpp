681c2b0157c74152bcffffe347d72a6831629adc
Roza Gkliva <roza.gkliva@ttu.ee>
2024-03-27 19:44:46 +0200

    Add parameters for hydromast

--- ros2_cppserial/src/serial_interface.cpp
+++ ros2_cppserial/src/serial_interface.cpp
@@ -21,6 +21,9 @@ class SerialInterface : public rclcpp::Node
             this->declare_parameter("incoming_message.header", "start");
             this->declare_parameter("incoming_message.packet_size", 0);
             this->declare_parameter("incoming_message.footer", "end");
+            this->declare_parameter("incoming_message.rate_hz", 0);
+            this->declare_parameter("publish_topic.name", "name");
+            this->declare_parameter("publish_topic.frame_id", "fid");
 
             std::string port = this->get_parameter("serial_params.port").as_string();
             int baud = this->get_parameter("serial_params.baud").as_int();
@@ -39,26 +42,32 @@ class SerialInterface : public rclcpp::Node
             std::string header = this->get_parameter("incoming_message.header").as_string();
             std::string footer = this->get_parameter("incoming_message.footer").as_string();
             int8_t packet_size = this->get_parameter("incoming_message.packet_size").as_int();
+            int8_t read_rate = this->get_parameter("incoming_message.rate_hz").as_int();
+            std::string topic_name = this->get_parameter("publish_topic.name").as_string();
+            frame_id_ = this->get_parameter("publish_topic.frame_id").as_string();
+
+            RCLCPP_INFO(this->get_logger(), "Header: %s, Footer: %s, Packet Size: %d, Read Rate: %d, Topic Name: %s, Frame ID: %s", header.c_str(), footer.c_str(), packet_size, read_rate, topic_name.c_str(), frame_id_.c_str());
 
             
 
             // create a timer to read from serial port
-            timer_ = this->create_wall_timer(std::chrono::milliseconds(20), std::bind(&SerialInterface::readSerial, this));
+            auto timer_period_ = 1/read_rate;
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(timer_period_), std::bind(&SerialInterface::readSerial, this));
 
             // create publisher 
-            publisher_ = this->create_publisher<ros2_serial_interfaces::msg::SerialString>("serial_data", 10);  // TODO: topic name from yaml
+            publisher_ = this->create_publisher<ros2_serial_interfaces::msg::SerialString>(topic_name, 10);  // TODO: topic name from yaml
 
             
         }
 
     private:
-        // define a struct to hold the parsed data
-        struct PayloadData
-        {
-            std::vector<std::string> content;
-            std::vector<int> varLens;
-            std::vector<std::variant<float, short>> variables;  // std::variant is a type-safe union to hold different types of variables
-        }; 
+        // // define a struct to hold the parsed data
+        // struct PayloadData
+        // {
+        //     std::vector<std::string> content;
+        //     std::vector<int> varLens;
+        //     std::vector<std::variant<float, short>> variables;  // std::variant is a type-safe union to hold different types of variables
+        // }; 
 
         rclcpp::Publisher<ros2_serial_interfaces::msg::SerialString>::SharedPtr publisher_;
         
@@ -68,7 +77,8 @@ class SerialInterface : public rclcpp::Node
             {
                 auto message = ros2_serial_interfaces::msg::SerialString();
                 message.header.stamp = this->get_clock()->now();
-                message.header.frame_id = "serial_data";        // TODO: frame_id from yaml
+                // use frame_id from node
+                message.header.frame_id = frame_id_; 
                 std::string data = serial_->readline();
 
                 message.data = data;
@@ -88,6 +98,7 @@ class SerialInterface : public rclcpp::Node
             }
         }
 
+        std::string frame_id_;
         std::shared_ptr<serial::Serial> serial_;
         rclcpp::TimerBase::SharedPtr timer_;
 
