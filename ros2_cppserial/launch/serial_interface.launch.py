ed528fcd53fe036bbf3a418e6c44ec8421317248
Roza Gkliva <roza.gkliva@ttu.ee>
2024-04-05 15:52:10 +0300

    Add second hydromast sensor
    - set serial ports, publisher topics, and id numbers for hydromasts
    - use hardware id to identify gps

--- ros2_cppserial/launch/serial_interface.launch.py
+++ ros2_cppserial/launch/serial_interface.launch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -10,22 +11,33 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     params_dir = get_package_share_directory('ros2_cppserial')
-    hydromast_params_file = os.path.join(params_dir, 'config', 'serial.yaml')
+    left_hydromast_params_file = os.path.join(params_dir, 'config', 'hydromast_n27.yaml')
+    right_hydromast_params_file = os.path.join(params_dir, 'config', 'hydromast_n26.yaml')
     gps_params_file = os.path.join(params_dir, 'config', 'gps.yaml')
+    yaml_files = [left_hydromast_params_file, right_hydromast_params_file, gps_params_file]
 
-    if os.path.exists(hydromast_params_file):
-        print("Found file: ", hydromast_params_file)
-    else:
-        print("File not found: ", hydromast_params_file)
+    for file in yaml_files:
+        if os.path.exists(file):
+            print("Found file: ", file)
+        else:
+            print("File not found: ", file)
+            sys.exit(1)
     
-    hydromast_interface_node = Node(
+    hydromast_left_interface_node = Node(
         package='ros2_cppserial',
         executable='serial_interface',
         name='hydromast_interface',
         output='both',
         emulate_tty=True,
-        # parameters=[{"port": "/dev/ttyACM0"}],
-        parameters=[hydromast_params_file],
+        parameters=[left_hydromast_params_file],
+    )        
+    hydromast_right_interface_node = Node(
+        package='ros2_cppserial',
+        executable='serial_interface',
+        name='hydromast_interface',
+        output='both',
+        emulate_tty=True,
+        parameters=[right_hydromast_params_file],
     )    
     gps_interface_node = Node(
         package='ros2_cppserial',
@@ -33,11 +45,11 @@ def generate_launch_description():
         name='gps_interface',
         output='both',
         emulate_tty=True,
-        # parameters=[{"port": "/dev/ttyACM0"}],
         parameters=[gps_params_file],
     )
 
     return LaunchDescription([
-        hydromast_interface_node,
-        # gps_interface_node,
+        hydromast_left_interface_node,
+        hydromast_right_interface_node,
+        gps_interface_node,
     ])
